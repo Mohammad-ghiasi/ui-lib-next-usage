78de8229e5f67a9d495c228e5fb8855f0e72fafc
Mohammad-ghiasi <mohammadghiasi005@gmail.com>
2025-04-16 19:56:52 +0330

    ui-lib-next-usage-2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Button } from "ghiasi-ui-library-kit";
 import { ShoppingCart } from "lucide-react";
-import Image from "next/image";
 
 export default function Home() {
   return (
